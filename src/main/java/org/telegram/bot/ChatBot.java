50c18517fec27c3c911bbd30c520410c937029cc
olehhordiienko-1 <116458066+olehhordiienko-1@users.noreply.github.com>
2022-10-24 19:00:35 +0300

    Clean junk, other commentaries etc.

--- src/main/java/org/telegram/bot/ChatBot.java
+++ src/main/java/org/telegram/bot/ChatBot.java
@@ -1,4 +1,4 @@
-package org.goiteens;
+package org.telegram.bot;
 
 import com.github.javafaker.Faker;
 
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class ChatBot {
     private static String randomDate;
     private static String randomFullName;
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String message = scanner.nextLine();
@@ -17,7 +17,7 @@ public class ChatBot {
         String botAnswer = process(message);
         System.out.println(botAnswer);
     }
-    
+
     private static void generateDate() {
 
         GregorianCalendar gregorianCalendar = new GregorianCalendar();
@@ -30,9 +30,7 @@ public class ChatBot {
 
         gregorianCalendar.set(Calendar.DAY_OF_YEAR, dayOfYear);
 
-        //System.out.println(gregorianCalendar.get(Calendar.YEAR) + "-" + (gregorianCalendar.get(Calendar.MONTH) + 1) + "-" + gregorianCalendar.get(gregorianCalendar.DAY_OF_MONTH));
-
-        randomDate = gregorianCalendar.get(Calendar.YEAR) + "-" + (gregorianCalendar.get(Calendar.MONTH) + 1) + "-" + gregorianCalendar.get(Calendar.DAY_OF_MONTH);
+        randomDate = gregorianCalendar.get(Calendar.YEAR) + "." + (gregorianCalendar.get(Calendar.MONTH) + 1) + "." + gregorianCalendar.get(Calendar.DAY_OF_MONTH);
     }
 
     private static int randBetween(int start, int end) {
@@ -44,52 +42,21 @@ public class ChatBot {
         randomFullName = faker.name().fullName();
     }
 
-
-    
     public static String process(String message) {
         if (isHelloMessage(message)) {
             String botName = "Random Date or Person Bot";
             return "Вітаю, я - " + botName;
-        } else if(isDateMessage(message)) {
+        } else if (isDateMessage(message)) {
             generateDate();
             return randomDate;
-        } else if(isPerson(message)) {
+        } else if (isPerson(message)) {
             generateFullName();
             return randomFullName;
         }
-//
-//        int professionSalary = find(message, professions);
-//        int dreamCost = find(message, dreams);
-//
-//        if (professionSalary < 0) {
-//            return "Я не знайшов у твоєму повідомленні назви професії";
-//        }
-//
-//        if (dreamCost < 0) {
-//            return "Я не знайшов у твоєму повідомленні мрії, яку ти хочеш";
-//        }
-//
-//        int monthCount = calculateMonthCount(dreamCost, professionSalary);
-//
-//        return "Щоб отримати свою мрію, потрібно місяців: " + monthCount;
+
         return "Enter /date or /person";
     }
 
-//    public static int find(String message, Map<String, Integer> data) {
-//        message = message.toLowerCase();
-//        String lowerCasedWord;
-//
-//        for(String word: data.keySet()) {
-//            lowerCasedWord = word.toLowerCase();
-//
-//            if (message.contains(lowerCasedWord)) {
-//                return data.get(word);
-//            }
-//        }
-//
-//        return -1;
-//    }
-    
     private static boolean isDateMessage(String message) {
         return message.equalsIgnoreCase("/date");
     }
@@ -97,7 +64,7 @@ public class ChatBot {
     private static boolean isPerson(String message) {
         return message.equalsIgnoreCase("/person");
     }
-    
+
     private static boolean isHelloMessage(String message) {
         message = message.toLowerCase();
 
